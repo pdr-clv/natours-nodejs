bb4a55d9362516cc9898413b92f0e54c9ada5a80
pdr-clv <pdr.clv@gmail.com>
2020-08-04 11:35:52 +0300

    Adding package moongose and database connection

--- app.js
+++ app.js
@@ -20,27 +20,24 @@ app.use(express.static(`${__dirname}/public`));
 
 //this is our own middleware function, we have to use params req, res, y next is obligatory, otherwise, never will go next step of middleware stack
 // if there is an response before middleware, never will be executed. it is better if they are on top.
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
 });
 //this middleare catch the url requested.
-app.use((req,res,next) => {
-  console.log('Current url requested: ',req.originalUrl);
+app.use((req, res, next) => {
+  console.log('Current url requested: ', req.originalUrl);
   next();
 });
 //this middleware add date/time of request
-app.use((req,res,next) => {
 //we define new property in request called req.requestTime
+app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-
-
 // 3. Mounting our routes
 //only for certain routes, they will be applied middleware, in this case are the routes.
-app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/users',userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
