ece0692045c62234700eceb6c6ab97e450e173f3
pdr-clv <pdr.clv@gmail.com>
2020-08-05 18:11:51 +0300

    refactoring and setting defaults values TourModel and new CRUD operations in MongoDb in tourController file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,106 +1,88 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-//2. Route handlers
-
-exports.checkID = (req,res,next,val) => {
-    //all values params in the url are stored in req.params, it is an object with the fields {id:'4'}
-  //we convert to number multiply by 1 
-  if (val*1 >= tours.length) {
-    return res.status(404).json({
-      status:'fail',
-      message:'Number tour not valid'
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'sucess',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
     });
   }
-  next();
 };
 
-exports.checkBody = (req,res,next) => {
-
-//we check if req.body object is empty
-    if (Object.keys(req.body).length === 0 ){
-      return res.status(400).json({
-        status:'fail',
-        message:"Bad request! Body can't be empty"
-      });
-//else, check if properties name and price are with data, obligatory fields.
-    } else if (!req.body.name || !req.body.price) {
-      return res.status(400).json({
-        status:'fail',
-        message:"Bad request! At least fields name and price must be filled with data"
-      });
-    }
-  next();
-}
-
-exports.getAllTours = (req,res) => {
-  console.log(req.requestTime);
-  res
-    .status(200)
-    .json({
-      status:'sucess',
-      requestedAt:req.requestTime,
-      results:tours.length,
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'sucess',
       data: {
-        tours
-      }
+        tour,
+      },
     });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req,res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-
-  res
-    .status(200)
-    .json({
-      status:'sucess',
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'sucess',
       data: {
-        tour
-      }
-    });  
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.createTour = (req,res) => {
-
-  const newId = tours[tours.length - 1].id + 1;
-//Object.assign can merge two objects in one
-  const newTour = Object.assign(
-    { id: newId },
-    req.body
-  );
-
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours), 
-    (err) => {
-      if (err) throw err;
-      res.status(201).json({
-        status:'sucess',
-        data:{
-          tour:newTour
-        }
-      });
-    }
-  );
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'sucess',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.updateTour = (req,res) => {
-
-  res.status(200).json({
-    status:'sucess',
-    data:'Data will be updated'
-  });
-
-};
-
-exports.deleteTour = (req,res) => {
-
-  res.status(204).json({
-    status:'sucess',
-    data:null
-  });
-
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'sucess',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,30 +1,23 @@
-
 const express = require('express');
 //we are going to create routers, it will replace app. using middleware, we will connect tourRouter with app.
 const tourController = require('../controllers/tourController');
 //we desectructure object controller. It has all functions of handlers of create, delete, etc.
-const { getAllTours, 
-  createTour, 
-  getTour, 
-  updateTour, 
-  deleteTour, 
-  checkID,
-  checkBody } = tourController;
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+} = tourController;
 
 const router = express.Router();
-
-//middleware for url params, in this case id. Also we can have access to a forth argument in a param middleware function.
-// with this middleware we can handle the id checking is in getUser, deleteUser and updateUser.One test for the three, we can delete all those id checking.
-router.param('id',checkID);
-//with ths middleware we want to check if the data we received in req.body is valid to to the post method for creating Tour
 
 router
   .route('/')
   .get(getAllTours)
-  .post(checkBody , createTour);
-// you can add previous functions before createTour, and this will be middleware previous to th function.
-// very interesting to add previous validations before executing any code.
-router
+  .post(createTour);
+
+  router
   .route('/:id')
   .get(getTour)
   .patch(updateTour)
