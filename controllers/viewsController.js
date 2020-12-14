22c5ca42ed747674aa068b420e1a674557b6e99a
pdr-clv <pdr.clv@gmail.com>
2020-12-14 19:17:27 +0300

    mapbox. How to render maps on the site

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('./errorController');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   //1. Get tour data from collection
@@ -21,8 +22,17 @@ exports.getTour = catchAsync(async (req, res, next) => {
   //2. Build template
 
   //3. Render template using data from step 1
-  res.status(200).render('tour', {
-    title: tour.name,
-    tour,
-  });
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: tour.name,
+      tour,
+    });
 });
---
+++ public/js/mapbox.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicGRyY2x2IiwiYSI6ImNrZWl4ZWIzNzBwaGYycXBkb3I1YnJnZmkifQ.Ic14uaLheQNgAca2badVUA';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/pdrclv/ckioor7s54zoi17qsizk5sy0r',
+  minZoom: 7,
+  //scrollZoom: false,
+  /*center:[-118.113491, 34.111745],
+  zoom: 9,
+  interactive: false,*/
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  }).setLngLat(loc.coordinates).addTo(map);
+
+  //Add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  }
+});
