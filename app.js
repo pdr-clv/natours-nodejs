0842c6e9b8f46136938ae8f1406cfd5c0f684723
pdr-clv <pdr.clv@gmail.com>
2020-12-23 20:04:52 +0300

    modifying headers in helmet middleware to avoid Content-Security-Policy in production

--- app.js
+++ app.js
@@ -30,8 +30,54 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Set Security HTTP Headers. Add new headers with more security details, following up secure practices
-app.use(helmet());
-
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
+        baseUri: ["'self'"],
+        fontSrc: ["'self'", 'https:', 'data:'],
+        scriptSrc: [
+          "'self'",
+          'https:',
+          'http:',
+          'blob:',
+          'https://*.mapbox.com',
+          'https://js.stripe.com',
+          'https://m.stripe.network',
+          'https://*.cloudflare.com',
+        ],
+        frameSrc: ["'self'", 'https://js.stripe.com'],
+        objectSrc: ["'none'"],
+        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+        workerSrc: [
+          "'self'",
+          'data:',
+          'blob:',
+          'https://*.tiles.mapbox.com',
+          'https://api.mapbox.com',
+          'https://events.mapbox.com',
+          'https://m.stripe.network',
+        ],
+        childSrc: ["'self'", 'blob:'],
+        imgSrc: ["'self'", 'data:', 'blob:'],
+        formAction: ["'self'"],
+        connectSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          'data:',
+          'blob:',
+          'https://*.stripe.com',
+          'https://*.mapbox.com',
+          'https://*.cloudflare.com/',
+          'https://bundle.js:*',
+          'ws://127.0.0.1:*/',
+        ],
+        upgradeInsecureRequests: [],
+      },
+    },
+  })
+);
 //Development logging
 //morgan is third part middleware which provides a console.log with few details of info. It has several arguments, interesting the one is 'dev'
 if (process.env.NODE_ENV === 'development') {
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Booking = require('../models/bookingModel');
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   //1. Get tour data from collection
   const tours = await Tour.find();
@@ -30,10 +31,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   //3. Render template using data from step 1
   res
     .status(200)
-    .set(
+    /*.set(
       'Content-Security-Policy',
       "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
+    )*/
     .render('tour', {
       title: tour.name,
       tour,
@@ -43,10 +44,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
 exports.getLoginForm = (req, res) => {
   res
     .status(200)
-    .set(
+    /*.set(
       'Content-Security-Policy',
       "connect-src 'self' https://cdnjs.cloudflare.com"
-    )
+    )*/
     .render('login', {
       title: 'Log into your account',
     });
